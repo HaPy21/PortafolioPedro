1056015366376f8a219e81c2d92405e7fe095702
HaPy21 <pluisha@outlook.es>
2024-03-22 19:05:59 -0500

    Inicio de algo grande

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings ">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      BackEnd Developer whit AWS
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+  justify-content: center;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,68 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Desarrollador FullStack en <a href="https://www.oneup-i.com/" target="_blank" rel="noopener"> <span class="text-green-500">OneUp</span></a></template>
+    <span class="opacity-85">Nov 2022 - May 2023</span> <br>
+    Desarrollo y soporte de requerimientos Landing Page, FTR y Addon Invoice. Soporte de sistema WebScraping y despliegue a producción (cloud).
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Practicante Pre Profesional en <a href="https://www.oneup-i.com/" target="_blank" rel="noopener"> <span class="text-green-500">OneUp</span></a></template>
+    <span class="opacity-85">Nov 2022 - May 2023</span> <br>
+    Soporte, gestión de Tickets, desarrollo e implementación de un módulo BI para el área de marketing de la empresa <a href="https://www.atsaairlines.com/" target="_blank" rel="noopener"> <span class="text-green-500">AtsaAirlines</span></a>
+    <br />
+
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>LAGONDA</template>
+    <span class="opacity-85">Mar 2023 - Jul 2023</span> <br>
+    Desarrollo de un Ecommerce aplicado a una empresa automotriz haciendo uso de servicios de Amazon Web
+    services aplicando el modelo PaaS.<br>
+    <template #logro >Top 2 en concurso de InfoFest | <a href="https://www.urp.edu.pe/" target="_blank" rel="noopener">Universidad Ricardo Palma</a> </template>
+
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>SISGRAPH</template>
+    <span class="opacity-85">Sept 2022 - Dic 2022</span> <br>
+      Parte del equipo de desarrollo para la gestión operativa, cotizaciones y ventas de la empresa SISGRAPH
+      implementando una arquitectura API Rest.<br>
+
+    <span class="mt-2 text-sm text-rose-500">Top 1 en concurso de InfoFest | <a href="https://www.urp.edu.pe/" target="_blank" rel="noopener">Universidad Ricardo Palma</a> </span>
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>TOQUE X</template>
+
+    Parte del equipo de desarrollo para la gestión de inventario, almacenes y ventas.
+    
+  </WelcomeItem>
+</template>
